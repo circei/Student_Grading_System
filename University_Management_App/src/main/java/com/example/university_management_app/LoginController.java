bf857db4ca8eeb46d13e92ba931d8ca614c430e8
Circei Andrei <andreicircei78@gmail.com>
2024-02-14 17:56:46 +0200

    created gui for login menu

---
+++ University_Management_App/src/main/java/com/example/university_management_app/LoginController.java
@@ -0,0 +1,10 @@
+package com.example.university_management_app;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
+public class LoginController {
+    public TextField usernameField;
+    public PasswordField passwordField;
+    
+
+}
